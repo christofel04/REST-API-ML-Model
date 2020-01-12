f2d8030faa67fa4e0a143b3c7465f0d1f933996b
Christofel Ganteng <47190511+christofel04@users.noreply.github.com>
2020-01-12 14:05:45 +0700

    Add files via upload

---
+++ MLRestAPI.py
@@ -0,0 +1,59 @@
+# Import needed package
+from flask import Flask # Package to web developing
+from flask_restful import reqparse, abort, Api, Resource # Package to make Restful API
+import pickle # Package to handling array in Python
+import numpy as np # Pake to apply ML model
+from model import NLPModel
+
+# Create 
+app = Flask(__name__)
+api = Api(app)
+
+model = NLPModel()
+
+clf_path = 'lib/models/SentimentClassifier.pkl'
+with open(clf_path, 'rb') as f:
+    model.clf = pickle.load(f)
+
+vec_path = 'lib/models/TFIDFVectorizer.pkl'
+with open(vec_path, 'rb') as f:
+    model.vectorizer = pickle.load(f)
+
+# argument parsing
+parser = reqparse.RequestParser()
+parser.add_argument('query')
+
+
+class PredictSentiment(Resource):
+    def get(self):
+        # use parser and find the user's query
+        args = parser.parse_args()
+        user_query = args['query']
+
+        # vectorize the user's query and make a prediction
+        uq_vectorized = model.vectorizer_transform(np.array([user_query]))
+        prediction = model.predict(uq_vectorized)
+        pred_proba = model.predict_proba(uq_vectorized)
+
+        # Output either 'Negative' or 'Positive' along with the score
+        if prediction == 0:
+            pred_text = 'Negative'
+        else:
+            pred_text = 'Positive'
+            
+        # round the predict proba value and set to new variable
+        confidence = round(pred_proba[0], 3)
+
+        # create JSON object
+        output = {'prediction': pred_text, 'confidence': confidence}
+        
+        return output
+
+
+# Setup the Api resource routing here
+# Route the URL to the resource
+api.add_resource(PredictSentiment, '/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
